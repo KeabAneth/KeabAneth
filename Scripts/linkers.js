4ca904eec72b89c411048a4b4f1e0922bb429776
Keab <137433322+KeabAneth@users.noreply.github.com>
2024-08-12 12:48:30 -0500

    update 3

--- Scripts/linkers.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("linkersCont").addEventListener("click", e => {
-    if (e.target.id === "backendLink") {
-        window.location.href = "/backend";
-    } else if (e.target.id === "frontendLink") {
-        window.location.href = "/frontend";
-    } else {
-        window.location.href = "/"
-    }
-})
